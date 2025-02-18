8e88740fd0119b09ac87736a90dc0103a7283dcb
YaelStO <yaelsantiago1108@gmail.com>
2025-02-18 08:39:11 -0600

    Primer commit

---
+++ Ac_fw.py
@@ -0,0 +1,14 @@
+# Programa para imprimir los primeros 10 números naturales utilizando while y for
+
+# Usando el bucle while
+print("Usando while:")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+print("\nUsando for:")
+
+# Usando el bucle for
+for j in range(1, 11):
+    print(j)
